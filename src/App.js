cf5b4ca010d39a4e0786f7c8f4f737d04f4654f3
Varun-18 <varunshah735@gmail.com>
2024-02-11 19:57:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [content, setContent] = useState("");
+  const [show, setShow] = useState(false);
+  const onClick = async () => {
+    // return await new Promise(async (res, rej) => {
+    //   setTimeout(async () => {
+    //     res(() => {
+    //       setContent("clicked");
+    //       setShow(true);
+    //     });
+    //   }, 2000);
+    // });
+
+    setShow(true);
+    setContent("clicked");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world..!</h1>
+      <button onClick={() => onClick()}>click here</button>
+      {show && <p data-testid="content">{content}</p>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("test group for snap shot testing", () => {
+  //   test("snapshot test 1", () => {
+  //     expect(render(<App />)).toMatchSnapshot();
+  //   });
+
+  test("getByText testing", () => {
+    render(<App />);
+    const textToTest = screen.getByText("Hello world..!");
+    expect(textToTest).toHaveTextContent("Hello world..!");
+  });
+
+  test("button click test", async () => {
+    render(<App />);
+    const btn = screen.getByRole("button");
+
+    fireEvent.click(btn);
+
+    const elem = screen.getByTestId("content");
+    expect(elem.textContent).toMatch("clicked");
+  });
 });
